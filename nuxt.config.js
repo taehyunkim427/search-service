90b9d946042391704d412e9bf71572672f251c0d
taehyunkim427 <thkim427@naver.com>
2023-03-31 06:19:19 +0900

    [deploy] vercel config

--- nuxt.config.js
+++ nuxt.config.js
@@ -79,9 +79,9 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 
-  routeRules:
+  /* routeRules:
    {  
   // Use client-side rendering for all routes
   '/**': { ssr: false },
-   },
+   }, */
 }
